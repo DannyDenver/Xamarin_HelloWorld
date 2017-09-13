3c4352ed3325c7d867bbb8a5b294abf40d6078e7
Daniel Taylor <djt09d@fulbrightmail.org>
2017-09-13 11:58:28 -0600

    Property Element syntax - cannot be represented in symple strings

--- HelloWorld2/HelloWorld2/GreetPage.xaml.cs
+++ HelloWorld2/HelloWorld2/GreetPage.xaml.cs
@@ -14,8 +14,42 @@ namespace HelloWorld2
 			InitializeComponent ();
 
             slider.Value = 0.5;
-            
-		}
+
+            if(Device.OS == TargetPlatform.iOS)
+            {
+                Padding = new Thickness(0, 20, 0, 0); // always add 20px to top of page 
+            }
+            else if (Device.OS == TargetPlatform.Android)
+            {
+                Padding = new Thickness(5, 20, 5, 5);
+            }
+
+            //Padding = Device.OnPlatform(    //depricated :(
+            //    iOS: new Thickness(0, 20, 0, 0),
+            //    Android: new Thickness(10, 20, 0, 0)
+            //    );
+
+            Device.OnPlatform(
+                iOS: () =>
+                {
+                    Padding = new Thickness(0, 20, 0, 0);
+                },
+                Android: () =>
+                {
+                    Padding = new Thickness(10, 10, 10, 10);
+                }) 
+
+        }
 
     }
 }
+
+
+
+//var x = new OnPlatform<Thickness>
+//{
+//    Android = new Thickness(0),
+//    iOS = new Thickness(0, 20, 0, 0)
+//};
+
+//Padding = x;
